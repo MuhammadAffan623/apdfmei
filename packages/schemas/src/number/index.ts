941dbde5b85bea5235a56313e047026014599a52
MuhammadAffan623 <affanmuhammad623@gmail.com>
2024-08-28 21:40:58 +0500

    updated package and stroke color

--- packages/schemas/src/number/index.ts
+++ packages/schemas/src/number/index.ts
@@ -11,8 +11,8 @@ const numberSchema: Plugin<TextSchema> = {
   // icon to update
   icon: `<svg width="24" height="24"  xmlns="http://www.w3.org/2000/svg">
   
-  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="16" fill="black">NO</text>
-  <rect x="50%" y="70%" width="25" height="1.5" style="fill:black;" />
+  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="16" fill="black">#</text>
+
 </svg>`,
 };
 
--- packages/schemas/src/time/index.ts
+++ packages/schemas/src/time/index.ts
@@ -9,7 +9,8 @@ const numberSchema: Plugin<TextSchema> = {
   ui: uiRender,
   propPanel,
   // icon to update
-  icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+  icon: `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
   <circle cx="12" cy="12" r="10" stroke="black" stroke-width="2" fill="none" />
   <line x1="12" y1="12" x2="12" y2="6" stroke="black" stroke-width="2" />
   <line x1="12" y1="12" x2="16" y2="14" stroke="black" stroke-width="2" />
